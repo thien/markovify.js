002287ffc147792a2825c28b4b63b42ce9e3fc33
jamjar919 <james@mjsquare.com>
2016-12-03 12:28:37 +0000

    frameork

---
+++ markovify/chain.js
@@ -0,0 +1,10 @@
+class Chain {
+    // data is array of strings
+    var corpus = {};
+    constuctor(data) {
+        for (var i = 0; i < data.length; i++) {
+            if (typeof corpus[data[i]]) {
+            }
+        }
+    }
+}
